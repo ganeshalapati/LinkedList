c4738eef5a09b053f76dc66ae84152e9add3e41e
ganeshalapati <alapatiganesh31@gmail.com>
2021-07-26 03:07:03 +0530

    Added method to append

--- MainList.java
+++ MainList.java
@@ -3,9 +3,9 @@ public class MainList {
 
 	public static void main(String[] args) {
 		LinkList linkList=new LinkList();
-		linkList.insert(70);
-		linkList.insert(30);
 		linkList.insert(56);
+		linkList.insert(30);
+		linkList.insert(70);
 		linkList.print();
 
 	}
