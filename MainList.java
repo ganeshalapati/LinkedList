754690132a1dc8de94d2dd55abb955ed172fadfe
ganeshalapati <alapatiganesh31@gmail.com>
2021-07-26 02:54:51 +0530

    Added methods for insertion

---
+++ MainList.java
@@ -0,0 +1,13 @@
+
+public class MainList {
+
+	public static void main(String[] args) {
+		LinkList linkList=new LinkList();
+		linkList.insert(70);
+		linkList.insert(30);
+		linkList.insert(56);
+		linkList.print();
+
+	}
+
+}
